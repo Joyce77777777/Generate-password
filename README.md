cf9587de4b418c4c3514b1046f11358792afa419
Joyce <ccata9328@gmail.com>
2023-11-29 23:05:36 -0500

    add all content

---
+++ README.md
@@ -0,0 +1,14 @@
+# JavaScript: Password Generator
+
+## Description
+
+This is to create an application that enables employees to generate random passwords based on criteria that they’ve selected. This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code. It will have a clean and polished, responsive user interface that adapts to multiple screen sizes.
+
+The password can include special characters. 
+
+## Screenshot
+
+./asset/screenshot.PNG
+
+
+## Link
---
+++ asset/script.js
@@ -0,0 +1,66 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Function to prompt the user for password criteria
+function getPasswordOptions() {
+  var length = parseInt(prompt("Enter the length of the password (8-128):"), 10);
+  var hasLowercase = confirm("Include lowercase letters?");
+  var hasUppercase = confirm("Include uppercase letters?");
+  var hasNumbers = confirm("Include numbers?");
+  var hasSpecialChars = confirm("Include special characters?");
+ 
+  // Validation
+  if (isNaN(length) || length < 8 || length > 128) {
+    alert("Password length must be a number between 8 and 128.");
+    return null;
+  }
+  if (!hasLowercase && !hasUppercase && !hasNumbers && !hasSpecialChars) {
+    alert("At least one character type must be selected.");
+    return null;
+  }
+
+  return {
+    length: length,
+    hasLowercase: hasLowercase,
+    hasUppercase: hasUppercase,
+    hasNumbers: hasNumbers,
+    hasSpecialChars: hasSpecialChars
+  };
+}
+
+
+// Function to generate a password
+function generatePassword() {
+  var options = getPasswordOptions();
+  if (options === null) {
+    return;
+  }
+  
+  var possibleChars = "";
+  if (options.hasLowercase) possibleChars += "abcdefghijklmnopqrstuvwxyz";
+  if (options.hasUppercase) possibleChars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if (options.hasNumbers) possibleChars += "0123456789";
+  if (options.hasSpecialChars) possibleChars += "!@#$%^&*()_+";
+
+  var password = "";
+  for (var i = 0; i < options.length; i++) {
+    var index = Math.floor(Math.random() * possibleChars.length);
+    password += possibleChars[index];
+  }
+  
+  return password;
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+
